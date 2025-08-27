311ebd4f4502cc7e5b33fa742d29d268fc45f926
Brandon-Lea Price <brandonlea810@gmail.com>
2025-08-27 21:03:26 +0100

    styled quiz.html and working on some Quiz JavaScript functions.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -19,6 +19,27 @@ function addScore(currentScore) {
 }
 
 // Getting data
-function getQuizData(level, category) {
+async function getQuizData(level, category) {
+    const url = "https://opentdb.com/api.php?amount=10&category=9";
+   
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
     
+        const result = await response.json();
+        console.log(result);
+      } catch (error) {
+        console.error(error.message);
+      }
+}
+
+function renderButton(text) {
+  var btn = document.createElement("button");
+
+  btn.innerText = text;
+  btn.className += "t";
+
+  return btn;
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
-const toggleNavbar = () => {
-    navLinks.classList.toggle('active');
-}
 
-//Navbar (Mobile/Tablet) Event
-hamburger.addEventListener("click", toggleNavbar());
+//Navbar (Mobile/Tablet)
+document.addEventListener('DOMContentLoaded', function() {
 
+    hamburger.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
